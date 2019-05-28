9b0ef85318ce4e84e21fe67b3573eebc3f4a76a9
Izzy761 <rplang761@gmail.com>
2019-05-28 10:24:27 -0700

    Delete PlayerHistory.sql

--- PlayerHistory.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PlayerHistory]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [PlayerID] INT NOT NULL, 
-    [Roll] INT NOT NULL DEFAULT 0 , 
-    [Point] INT NULL  , 
-    [Result] NVARCHAR(4) NULL   , 
-    CONSTRAINT [FK_PlayerHistory_ToTable] FOREIGN KEY (PlayerID) REFERENCES [Players](ID)
-)
